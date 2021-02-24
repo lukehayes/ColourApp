282e409aa4889fcf0e3bbd57f0456c2524b665de
lukehayes <lukepurplehayes@gmail.com>
2021-02-24 11:35:29 +0000

    First Commot

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Colours</title>
+</head>
+<body>
+    
+
+<script charset="utf-8" src="js/main.js"></script>
+</body>
+</html>
+
