c85d058675b8e7449a1f914a8276e35bd7c66922
lukehayes <lukepurplehayes@gmail.com>
2021-02-24 17:45:50 +0000

    Javascript now generating markup, colors etc

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import {upto,randomBetween} from "./math.js"
+import { createTag, createDiv} from "./UI.js"
+
+let root = document.getElementById("app");
+
+let colors = ["#f4f5f7", "#f2d1d4", "#e3e5e6"];
+let colorPick = colors[randomBetween(0,3)];
+
+root.style.backgroundColor = colors[randomBetween(0,3)];
+
+createDiv({
+    value: colorPick,
+    parent: root,
+    class: ['container', 'text-large']
+});
 
 
-upto(5).map( () => console.log( randomBetween(20,30) ) );
 
