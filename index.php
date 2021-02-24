59869fe80035abd2d02223d03c31439a667457d0
lukehayes <lukepurplehayes@gmail.com>
2021-02-24 17:46:28 +0000

    Added stylesheet to index file

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Colours</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body id="app">
-    
 
-<script charset="utf-8" type="module" src="src/main.js"></script>
+
+    <script charset="utf-8" type="module" src="src/main.js"></script>
 </body>
 </html>
 
