d49cb9dbb83fc3d574d3de9245a2af0a769ff62e
lukehayes <lukepurplehayes@gmail.com>
2021-02-24 15:32:49 +0000

    Added randomBetween function

--- src/random.js
+++ src/random.js
@@ -10,5 +10,24 @@ function upto(n)
     return [...Array(n).keys()];
 }
 
-export { upto };
+
+/**
+ * Return a random integer between min and max - excluding max.
+ *
+ * @param {number} min    The minium number to to start with.
+ * @param {number} max    The maximum number to reach.
+ * @return {number}       A number with in the range min - max.
+ */
+function randomBetween(min, max)
+{
+    let result = Math.floor(Math.random() * (max - min)) + min;
+
+    return result;
+}
+
+
+export {
+    upto,
+    randomBetween
+};
 
