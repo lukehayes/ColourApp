e5464ae21991f5c73a3aa07a007d47b57a5ce738
lukehayes <lukepurplehayes@gmail.com>
2021-02-25 17:00:45 +0000

    Added display color box creation

--- src/main.js
+++ src/main.js
@@ -11,4 +11,24 @@ createDiv({
     class: ['container', 'text-large']
 });
 
+let wrapper = createDiv({
+    value: null,
+    parent: root,
+    class: ['container']
+});
+
+let display = createDiv({
+    value: null,
+    parent: wrapper,
+    class: ['display']
+});
+
+for(let i = 0; i<=4; i++)
+{
+    createDiv({
+        value: null,
+        parent: display,
+        class: ['color-box']
+    });
+}
 
